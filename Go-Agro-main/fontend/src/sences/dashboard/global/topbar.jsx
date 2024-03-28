ea7af7f644f9456e7d2aa3ab4b9e59d889be6386
Naveen Nuwantha <naveen.nuwantha076@gmail.com>
2024-03-28 11:47:32 +0530

    created this parts

---
+++ Go-Agro-main/fontend/src/sences/dashboard/global/topbar.jsx
@@ -0,0 +1,7 @@
+const topbar = () => {
+    return <div>topbar</div>
+
+
+}
+
+export default topbara;
---
+++ Go-Agro-main/fontend/src/theme.js
@@ -0,0 +1,8 @@
+import {createContext, useState, useMemo} from "react";
+import { createTheme } from "@mui/material/styles";
+
+
+//color design tokens
+
+#1A4133
+#FFFFFF
