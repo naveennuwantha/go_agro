24d35d8ffbf3f0a59a3c70ddaadd4231b2304059
Kaveesha Nethmi <nethmikavee40@gmail.com>
2024-04-03 12:52:15 +0530

    Update reviewRoute.js

--- Go-Agro-main/backend/router/reviewRoute.js
+++ Go-Agro-main/backend/router/reviewRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {Review} from '../models/reviewModel.js';
+import {Review} from '../modle/reviewModel.js';
 const router = express.Router();
 
 //Route for save a new review
