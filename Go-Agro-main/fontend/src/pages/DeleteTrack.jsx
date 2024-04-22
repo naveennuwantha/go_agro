7fd62b7ddd0e1a08a8067d2065f9217faa2ef6aa
Hasandara Sandamini <hasandarasandamini164@gmail.com>
2024-04-22 19:59:18 +0530

    edit

--- Go-Agro-main/fontend/src/pages/DeleteTrack.jsx
+++ Go-Agro-main/fontend/src/pages/DeleteTrack.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import axios from 'axios';
+import { useNavigate, useParams} from 'react-router-dom';
+import { useSnackbar } from "notistack";
 
-const DeleteTrack = () => {
+const DeleteTrack = () =>{
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate() ;
+    const { id } = useParams();
+    const { enqueueSnackbar } = useSnackbar();
+    const handleDeleteTrack = () =>{
+        setLoading(true);
+        axios
+        .delete(`http://localhost:5000/tracks/${id}`)
+        .then(() =>{
+            setLoading(false);
+            enqueueSnackbar('Track Deleted successfully',{ variant: 'success'});
+            navigate('/');
+        })
+        .catch((error) =>{
+            setLoading(false);
+           // alert('An error happened.Please check consoole');
+            console.log(error);
+            enqueueSnackbar('Error',{ variant: 'error'});
+        });
+    };
+
   return (
-    <div>
+    <div className="p-4"><BackButton/>
+    <h1 className="text-3xl my-4"> Delete Track</h1>
+    {loading ? <Spinner/> : ''}
+    <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are You Sure You Want to Delete this Track?</h3>
+
+        <button
+        className="p-4 bg-red-600 text-white m-8 w-full"
+        onClick={handleDeleteTrack}>
+            Yes,Delete it
+        </button>
+    </div>
       
     </div>
   )
--- Go-Agro-main/fontend/src/pages/Home.jsx
+++ Go-Agro-main/fontend/src/pages/Home.jsx
@@ -6,83 +6,53 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
+
+
 const Home = () => {
     const [tracks, setTracks] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [showType, setShowType] = useState('table');
     useEffect(() => {
         setLoading(true);
         axios
-        .get("http://localhost:5000/tracks")
-        .then((response) => {
-        setTracks(response.data.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setLoading(false);
-      });
-  }, []);
-   
+            .get("http://localhost:5000/tracks")
+            .then((response) => {
+                setTracks(response.data.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+            });
+    }, []);
 
-    return(
+
+    return (
         <div className="p-4">
-        <div className="flex justify-betwen items-center">
-            <h1 className="text-3xl my-8">Tracks List</h1>
-            <Link to='/tracks/create'>
-                <MdOutlineAddBox className="text-sky-800 text-4xl"/>
-            </Link>
+        <div className="flex justify-center items-center gap-x-4">
+          <button
+            className="bg-sky-300 hover:bg-sky-600 px-4 py-1"
+            onClick={() => setCreateType('table')}
+          >
+            Table
+          </button>
+          <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1"
+          onClick={() => setCreateType('card')}
+        >
+          Card
+        </button>
+          /</div>
+            <div className="flex justify-betwen items-center">
+                <h1 className="text-3xl my-8">Order Tracking List</h1>
+                <Link to='/tracks/create'>
+                    <MdOutlineAddBox className="text-sky-800 text-4xl" />
+                </Link>
             </div>
-            {loading ?(
-                <Spinner/>
-            ) : (
-                <table className="w-full border-separate border-spacing-2">
-                    <thead>
-                    <tr>
-                    <th className="border border-slate-600 rounded-md">No</th>
-                    <th className="border border-slate-600 rounded-md">Order ID</th>
-                    <th className="border border-slate-600 rounded-md max-md:hidden">Address</th>
-                    <th className="border border-slate-600 rounded-md">Status</th>
-                    <th className="border border-slate-600 rounded-md">Operations</th>
-
-      </tr>
-
-                    </thead>
-                    <tbody>
-                    {tracks.map((track, index) => (
-        <tr key={track._id} className='h-8'>
-          <td className="border border-slate-700 rounded-md text-center">
-            {index + 1}
-          </td>
-          <td className="border border-slate-700 rounded-md text-center">
-            {track.OrderId}
-          </td>
-          <td className="border border-slate-700 rounded-md text-center">
-            {track.address}
-          </td>
-          <td className="border border-slate-700 rounded-md text-center">
-            {track.status}
-          </td>
-         
-          <td className="border border-slate-700 rounded-md text-center">
-            <div className="flex justify-center gap-x-4">
-              <Link to={`/tracks/details/${track._id}`}>
-                <BsInfoCircle className="text-2xl text-green-800" />
-              </Link>
-              <Link to={`/tracks/Edit/${track._id}`}>
-                <AiOutlineEdit className="text-2xl text-yellow-800" />
-              </Link>
-              <Link to={`/tracks/delete/${track._id}`}>
-                <MdOutlineDelete className="text-2xl text-red-800" />
-              </Link>
-              </div>
-              </td>
-              </tr>
-                    ))}
-
-                    </tbody>
-                </table>
-                )}
-            </div>
+            {loading ? <Spinner/> :<TracksTable tracks={tracks}/>}
+               
+          
+        </div>
     )
 }
 export default Home
