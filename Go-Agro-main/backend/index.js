234fd8c51444774444d44d81a1b5eccdb3308747
Ishini Tecla Warnasooriya <teclaishini442@gmail.com>
2024-04-22 14:18:20 +0530

    Add files via upload
    add index.js

--- Go-Agro-main/backend/index.js
+++ Go-Agro-main/backend/index.js
@@ -2,42 +2,56 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import complaintRoute from './router/ComplaintRoute.js'
+import complaintRoute from './router/ComplaintRoute.js';
+import farmerRoute from './router/FarmerRoute.js';
+import listRoute from './router/listRoute.js';
 import bodyParser from 'body-parser';
-import farmerRoute from './router/FarmerRoute.js'
+import multer from 'multer';
 
-app.use('/farmers', farmerRoute )
+dotenv.config();
 
-dotenv.config()
+const app = express();
+const port = process.env.PORT || 8000;
 
-const app = express()
-const port = process.env.PORT || 8000
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('uploads'));
 
-
-app.use(cors())
 
 const corsOption = {
     origin:true
-}
+};
 
-app.listen(port,()=>{
-    console.log('server is running '+port)
-})
+app.listen(port, () => {
+    console.log('server is running ' + port);
+});
 
-//database connection
 const URL = process.env.MONGO_URL;
 
+mongoose.connect(URL); 
 
-mongoose.connect(URL, {
- 
-}); 
+const storage = multer.diskStorage({
+
+    destination:(req,file,cb) =>{
+        cb(null,'uploads')
+    },
+    filename:(req,file,cb) =>{
+        cb(null,file.originalname);
+    }
+});
+
+const upload = multer({
+    storage:storage
+})
 
 const connection = mongoose.connection;
 connection.once("open", () =>{
-    console.log("MongoDb connection sucessfull");
-})
+    console.log("MongoDb connection successful");
+});
 
 
-app.use('/complaints', complaintRoute )
-app.use('/farmers', )
+// Routes
+app.use('/complaints', complaintRoute);
+app.use('/farmers', farmerRoute);
+app.use('/lists', listRoute);
